7a2a69a5755c344de1aad15ead62b99bb6e0c063
Ben Pollack <benpollack8723@gmail.com>
2024-06-06 17:22:49 -0500

    started coding files

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const searchDropdown = document.getElementById('section');
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
 
+const showInput = (value) =>{
+let value = searchDropdown.value;
+searchInput.style.display = "block";
+searchBtn.style.display = "block";
+
+}
+
+searchDropdown.addEventListener('select', showInput);
